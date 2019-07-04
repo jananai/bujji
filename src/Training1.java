4ce9b945c3a7abd507e778cc8cddec570caa9b33
jananai <44000854+jananai@users.noreply.github.com>
2019-07-04 15:09:38 +0530

    Update Training1.java

--- src/Training1.java
+++ src/Training1.java
@@ -1,7 +1,9 @@
 
 public class Training1 {
 System.out.println("madhu");
-  System.out.println("madhu");
+  System.out.println("nandhu");
   
-  System.out.println("madhu");
+  System.out.println("vandhu");
+  System.out.println("vishnu");
+  System.out.println("bindhu");
 }
